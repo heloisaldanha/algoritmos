90e2f0bf34107e8f5569ee73db38fd5c3216116b
heloisaldanha <heloisa.saldanha@gmail.com>
2024-03-03 14:42:28 -0300

    ajusta erro em sortedBy

--- src/main/kotlin/fundamentos/SortedBy.kt
+++ src/main/kotlin/fundamentos/SortedBy.kt
@@ -5,5 +5,4 @@ fun main() {
     println(nomes.sortedBy { it })  // vai ordenar pela primeira letra
     println(nomes.sortedBy { it.length })  // vai ordenar pela quantidade de letras
     println(nomes.sortedBy { it.reversed() })  // vai inverter a palavra e pegar pela ordem da última letra
-    println(nomes.sortedBy { )
 }
